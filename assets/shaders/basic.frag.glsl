e99631861d53175a0eb436070a4920e90654a5ad
iWas-Coder <wasymatieh01@gmail.com>
2024-07-02 13:03:12 +0200

    Uniforms

--- assets/shaders/basic.frag.glsl
+++ assets/shaders/basic.frag.glsl
@@ -1,7 +1,9 @@
 #version 330 core
 
-layout(location = 0) out vec4 color;
+layout(location = 0) out vec4 o_Color;
+
+uniform vec4 u_Color;
 
 void main(void) {
-  color = vec4(0.2, 0.3, 0.8, 1.0);
+  o_Color = u_Color;
 }
